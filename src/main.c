1e56941bc520d9242d6da09614a5f199c7e085e0
stephanoumenos <s_dechecchi@hotmail.com>
2017-10-19 20:21:58 -0200

    Consertar grep

--- src/main.c
+++ src/main.c
@@ -1,17 +1,11 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
-
 #include <stdio.h>
 
 int main()
 {
-  int x, y;
-
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+    // remove números pares: cat test04.in | grep .*[13579]$
+    FILE* input = popen("grep .*[13579]$", "r");
+    char buffer[1024];
+    while(fgets(buffer, 1024, input))
+        printf("%s\n", buffer);
+    return 0;
 }
